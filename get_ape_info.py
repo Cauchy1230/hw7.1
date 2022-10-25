3c087914d6de739bea73fc8b8fc67590da805f08
yifengx <xionfyf1230@gmail.com>
2022-10-25 11:43:02 -0700

    add new code

--- get_ape_info.py
+++ get_ape_info.py
@@ -8,73 +8,43 @@ import time
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.toChecksumAddress(bayc_address)
 
-# You will need the ABI to connect to the contract
-# The file 'abi.json' has the ABI for the bored ape contract
-# In general, you can get contract ABIs from etherscan
-# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+#You will need the ABI to connect to the contract
+#The file 'abi.json' has the ABI for the bored ape contract
+#In general, you can get contract ABIs from etherscan
+#https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
 with open('/home/codio/workspace/abi.json', 'r') as f:
 	abi = json.load(f)
 
 ############################
-# Connect to an Ethereum node
-token = "Mwb3juVAfI1g2RmA1JCGdYk-2_BmFrnLOtbomP1oDa4"
-api_url = f"https://c2emjgrvmi7cabd41mpg.bdnodes.net?auth={token}"
+#Connect to an Ethereum node
+api_url = f"https://mainnet.infura.io/v3/e4d1bf289f074dd5ad05e74cb57a4332"
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
-
 def get_ape_info(apeID):
+	assert isinstance(apeID,int), f"{apeID} is not an int"
+	assert 1 <= apeID, f"{apeID} must be at least 1"
 
-    token = "Mwb3juVAfI1g2RmA1JCGdYk-2_BmFrnLOtbomP1oDa4"
-    api_url = f"https://c2emjgrvmi7cabd41mpg.bdnodes.net?auth={token}"
-    provider = HTTPProvider(api_url)
-    web3 = Web3(provider)
+	data = {'owner': "", 'image': "", 'eyes': "" }
 
-    assert isinstance(apeID, int), f"{apeID} is not an int"
-    assert 1 <= apeID, f"{apeID} must be at least 1"
+	#YOUR CODE HERE
 
-    data = {'owner': "", 'image': "", 'eyes': ""}
+	contract = web3.eth.contract(address=contract_address, abi=abi)
 
-    # YOUR CODE HERE
+	data['owner'] = contract.functions.ownerOf(apeID).call()
 
-    url = r"https://gateway.pinata.cloud/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/"
-    url_content = url + str(apeID)
-    request = requests.get(url_content)
-    data_1 = request.json()
-    image = data_1['image']
-    attributes = data_1['image']
-    image = data_1['image']
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat?arg=QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/'+ str(apeID), auth =  ('2BtY3QjKPh6aC1pUxDSM0w1W4hg', '5e24ddc1ecfcddfba6cadcaad1fb61dd'))
 
+	responsedata = json.loads(response.text)
 
-    for attr in data_1['attributes']:
+	data['image'] = responsedata['image']
 
-        if attr['trait_type'] == 'Eyes':
-            eyes = attr['value']
+	for instance in responsedata['attributes']:
+		if instance['trait_type'] == 'Eyes':
+			data['eyes'] = instance['value']
+			break
 
-
-
-
-
-
-    contract = web3.eth.contract(address=contract_address, abi=abi)
-    owner = contract.functions.ownerOf(apeID).call()
-
-    data['owner'] = owner
-    data['image'] = image
-    data['eyes'] = eyes
-
-
-    assert isinstance(data, dict), f'get_ape_info{apeID} should return a dict'
-    assert all([a in data.keys() for a in
-                ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
-    print(data['owner'],data['image'],data['eyes'] )
-    return data
-
-def main():
-    bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-    get_ape_info(6335)
-
-
-if __name__ == '__main__':
-    main()
+	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict'
+	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
+	return data
 
